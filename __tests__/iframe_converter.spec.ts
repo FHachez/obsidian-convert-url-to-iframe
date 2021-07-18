8dcd0ee2aea7d9db7dd5167cdffb301156a1ebaf
Floran Hachez <floran.hachez@gmail.com>
2021-07-18 11:23:20 +0200

    Preserve youtube timestamp (#21)
    * preserve timestamp
    
    * Update release notes
    
    Co-authored-by: Floran Hachez <floranhachez@rosa.be>

--- __tests__/iframe_converter.spec.ts
+++ __tests__/iframe_converter.spec.ts
@@ -3,9 +3,13 @@ import { isUrl, updateUrlIfYoutube } from '../iframe_converter';
 describe('updateUrlIfYoutube', () => {
 	const inputToExpectedOutput = [
 		["https://github.com/", "https://github.com/"],
+		["https://www.youtube.com/embed/FY7D_KM-xBw", "https://www.youtube.com/embed/FY7D_KM-xBw"],
 		["https://www.youtube.com/watch?v=FY7DtKMBxBw", "https://www.youtube.com/embed/FY7DtKMBxBw"],
+		["https://www.youtube.co.uk/watch?v=FY7DtKMBxBw", "https://www.youtube.co.uk/embed/FY7DtKMBxBw"],
 		["https://www.youtube.com/watch?v=FY7D_KM-xBw", "https://www.youtube.com/embed/FY7D_KM-xBw"],
 		["https://www.youtube.com/embed/FY7DtKMBxBw", "https://www.youtube.com/embed/FY7DtKMBxBw"],
+		["https://youtu.be/FKh8hjJNhWc?t=10027", "https://www.youtube.be/embed/FKh8hjJNhWc?start=10027"],
+		["https://youtu.co.uk/FKh8hjJNhWc?t=10027", "https://www.youtube.co.uk/embed/FKh8hjJNhWc?start=10027"],
 		["https://www.youtube.com/watch?v=DLMkT_FHczU&ab_channel=HughJeffreysHughJeffreys", "https://www.youtube.com/embed/DLMkT_FHczU"]
 	]
 	it.each(inputToExpectedOutput)('should correctly parse "%s"', (input: string, expected) => {
--- iframe_converter.ts
+++ iframe_converter.ts
@@ -1,16 +1,37 @@
 
 
 export function updateUrlIfYoutube(url: string) {
-    var youtubeIdRegex = /(youtube\.com\/([^\/]+\/.+\/|(v|e(mbed)?)\/|.*[?&]v=)|youtu\.be\/)(?<id>[a-zA-Z0-9_-]{11})/gi;
-    var id = youtubeIdRegex.exec(url)
+    // Youtube url format: 
+    // - youtube.com/.../?...&v=[Video Id]
+    // - youtu.com/[Video Id]
+    // - youtube.com/embed => don't do anything it's already an embed link
+    const youtubeIdRegex = /(youtube(?<top_domain1>(\.\w{2,3}){1,2})\/([^\/]+\/.+\/|.*[?&]v=)|youtu(?<top_domain2>(\.\w{2,3}){1,2})\/)(?<id>[a-zA-Z0-9_-]{11})/gi;
+    const id = youtubeIdRegex.exec(url)
+
     if (id) {
-        return `https://www.youtube.com/embed/${id.groups['id']}`;
+        const urlParameterMatches = url.match(/\?([^&]+)/);
+        const urlParameters = new URLSearchParams(
+            urlParameterMatches ? urlParameterMatches[0] : ''
+        )
+
+        // We need to remove the `v` parameter which contains the video id since we put it in the url
+        urlParameters.delete('v')
+
+        if (urlParameters.has('t')) {
+            urlParameters.set('start', urlParameters.get('t'));
+            urlParameters.delete('t')
+        }
+
+        const parameters = urlParameters.toString().length > 0 ? `?${urlParameters.toString()}` : '';
+        const topDomain = id.groups['top_domain1'] || id.groups['top_domain2'] || ".com";
+
+        return `https://www.youtube${topDomain}/embed/${id.groups['id']}${parameters}`;
     }
     return url;
 }
 
 export function isUrl(text: string): boolean {
-    let urlRegex = new RegExp(
+    const urlRegex = new RegExp(
         "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$"
     );
     return urlRegex.test(text);
